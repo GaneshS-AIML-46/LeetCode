2b553ce72ee80cd4655f97091fd345422976f3b9
Ganesh S <231501046@rajalakshmi.edu.in>
2026-02-05 23:19:56 +0530

    Time: 1 ms (94.66%) | Memory: 45.6 MB (49.84%) - LeetSync

---
+++ 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean canAliceWin(int[] nums) {
+    int s= 0, d = 0;
+
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] <= 9){
+                s += nums[i];
+            }else{
+                d += nums[i];
+            }
+        }
+
+        return (s > d) || (s < d);    
+    }
+}
