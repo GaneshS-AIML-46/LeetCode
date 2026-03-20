4a69b3460c1c6241d4d81b887cbc31a6c909bb9c
Ganesh S <231501046@rajalakshmi.edu.in>
2026-03-20 18:08:50 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (83.45%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n==1)
+        return n;
+
+        int f=1;
+        int s=2;
+        for(int i=3;i<=n;i++)
+        {
+            int ans=f+s;
+            f=s;
+            s=ans;
+
+        }
+        return s;
+    }
+}
