2e5c6d733076ce10e2ee0d7eaaacb9a51f294634
Ganesh S <231501046@rajalakshmi.edu.in>
2026-03-30 22:12:23 +0530

    Time: 2 ms (89.81%) | Memory: 106.7 MB (99.46%) - LeetSync

---
+++ 2519-find-the-original-array-of-prefix-xor/find-the-original-array-of-prefix-xor.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] findArray(int[] pref) {
+        int []arr=new int[pref.length];
+        
+
+        for(int i=pref.length-1;i>0;--i)
+        {
+           pref[i]=pref[i]^pref[i-1];
+        }
+        return pref;
+    }
+}
