a22eae0ea232f7e1df7cd25819f6df7aefd732b4
Ganesh S <231501046@rajalakshmi.edu.in>
2026-04-02 22:15:31 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (23.97%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.java
+++ 371-sum-of-two-integers/sum-of-two-integers.java
@@ -1,5 +1,13 @@
 class Solution {
     public int getSum(int a, int b) {
-       return (a+b); 
+      int sum=0;
+      while(b!=0){
+        sum=a&b;
+        a=a^b;
+        b=sum<<1;
+      }
+      return a;
+
+
     }
 }
