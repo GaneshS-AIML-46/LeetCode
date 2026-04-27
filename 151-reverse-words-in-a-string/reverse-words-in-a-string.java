03d62ffbd915c745068ed0c7ddf60af2a4dc1821
Ganesh S <231501046@rajalakshmi.edu.in>
2026-04-27 22:25:48 +0530

    Update reverse-words-in-a-string.java

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -3,7 +3,7 @@ class Solution {
       s=s.trim();
       List<String> list=Arrays.asList(s.split("\s+"));
             Collections.reverse(list);
-
+// reverse return
         return String.join(" ",list);
 
     }
