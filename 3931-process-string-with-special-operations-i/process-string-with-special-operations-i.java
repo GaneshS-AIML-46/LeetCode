dddcaf729b723618a27d96c31a74c661127f8d49
Ganesh S <231501046@rajalakshmi.edu.in>
2026-06-16 09:05:32 +0530

    Time: 4 ms (76.47%) | Memory: 55.5 MB (36.76%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder sb=new StringBuilder();
+        for(char c:s.toCharArray())
+        {
+            if(c=='*'){
+                if(sb.length()>0)
+
+               sb.deleteCharAt((sb.length())-1);
+            }
+
+            else if(c=='#')
+            sb.append(sb);
+            
+            else if(c=='%')
+            sb.reverse();
+            
+            else
+            sb.append(c);
+
+
+        }
+
+        String ans=sb.toString();
+
+        return ans;
+
+    }
+}
